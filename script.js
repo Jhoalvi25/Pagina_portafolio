b85804ca34317bfc29964ab3f5b8d9368a706960
Jhoalvi pereira <101520054+Jhoalvi25@users.noreply.github.com>
2023-01-20 09:41:58 -0500

    modificacion cv y pro

--- script.js
+++ script.js
@@ -24,23 +24,24 @@ function responsiveMenu() {
 
 // scroll
 
-window.onscroll = function(){
-    efectoHabilidades();
-}
+window.onscroll = function () {
+  efectoHabilidades();
+};
 
 // animacion de scroll
 
-function efectoHabilidades(){
-    var skills = getElementById("skills");
-    var distancia_skills = window.innerHeight = skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("react").classList.add("barra-progreso3");
-        document.getElementById("vue").classList.add("barra-progreso4");
-        document.getElementById("node").classList.add("barra-progreso5");
-        document.getElementById("express").classList.add("barra-progreso6");
-        document.getElementById("sql").classList.add("barra-progreso7");
-        document.getElementById("word").classList.add("barra-progreso8");
-    }
+function efectoHabilidades() {
+  var skills = getElementById("skills");
+  var distancia_skills = (window.innerHeight =
+    skills.getBoundingClientRect().top);
+  if (distancia_skills >= 300) {
+    document.getElementById("html").classList.add("barra-progreso1");
+    document.getElementById("js").classList.add("barra-progreso2");
+    document.getElementById("react").classList.add("barra-progreso3");
+    document.getElementById("vue").classList.add("barra-progreso4");
+    document.getElementById("node").classList.add("barra-progreso5");
+    document.getElementById("express").classList.add("barra-progreso6");
+    document.getElementById("sql").classList.add("barra-progreso7");
+    document.getElementById("word").classList.add("barra-progreso8");
+  }
 }
